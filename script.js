4b44af809075ec770bea4a2b5fcf2ed0815a4586
Shazaiiiib7 <143645351+Shazaiiiib7@users.noreply.github.com>
2023-11-01 19:51:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function page1animation(){
+    var imgArray = ["https://c4.wallpaperflare.com/wallpaper/338/209/238/girl-guitar-game-face-wallpaper-preview.jpg",
+"https://cdn.wallpapersafari.com/99/81/t0PGKX.jpg","https://cdn.wallpapersafari.com/92/89/S1KxLt.jpg"]
+    var container = document.querySelector(".page-1 .pic-container");
+
+}
+page1animation();
